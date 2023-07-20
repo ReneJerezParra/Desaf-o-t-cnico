dfaaf9497b4f32d9a921e9ab93b075ce315afbcc
ReneJerezParra <renejerezparra@gmail.com>
2023-07-20 03:15:15 +0100

    add_table_and_validations

--- src/component/GitHubUser.jsx
+++ src/component/GitHubUser.jsx
@@ -14,9 +14,14 @@ import { useState } from "react";
 import axios from "axios";
 import { HandleScreenReducer } from "../reducers/HandleScreenReducer";
 import Typography from "@mui/material/Typography";
+import TableRepos from "../utiles/tabla";
 
 export default function GitHubUser() {
   const [userName, setUserName] = useState("");
+  const [err, setErr] = useState({
+    error: false,
+    message: "",
+  });
 
   const [{ loading, error, data }, dispatch] = useReducer(HandleScreenReducer, {
     data: {},
@@ -24,9 +29,29 @@ export default function GitHubUser() {
     error: "",
   });
 
+  const validateUserName = (userName) => {
+    const regExp = /^[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,37}[a-zA-Z0-9])?$/;
+    return regExp.test(userName);
+  };
+  const badRequest = (error) => {
+    const regExp = /\b4\d{2}\b/;
+    return regExp.test(error);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    getData();
+    if (!validateUserName(userName)) {
+      setErr({
+        error: true,
+        message: "Incorrect GitHub user format",
+      });
+    } else {
+      setErr({
+        error: false,
+        message: "",
+      });
+      getData();
+    }
   };
 
   const getData = async () => {
@@ -35,17 +60,26 @@ export default function GitHubUser() {
       const result = await axios.get(
         `https://api.github.com/users/${userName}`
       );
+
+      const list_repos_url = result.data.repos_url;
+      const list_repos_response = await axios.get(list_repos_url);
+
+      const updated_list_repos = list_repos_response.data.map((repo) => {
+        const dateString = repo.created_at.slice(0, 10);
+        return { ...repo, created_at: dateString };
+      });
+
+      result.data.list_repos = updated_list_repos;
+
       dispatch({
         type: "FETCH_REQUEST_SUCCESS",
         payload: result.data,
       });
-      console.log(data);
     } catch (err) {
       dispatch({ type: "FETCH_FAIL", payload: err.message });
     }
   };
 
-  console.log(data);
   return (
     <div>
       <Box component="form" onSubmit={handleSubmit}>
@@ -55,8 +89,11 @@ export default function GitHubUser() {
           type="string"
           variant="outlined"
           size="small"
+          required
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
+          error={err.error}
+          helperText={err.message}
         />
         <IconButton type="submit" sx={{ ml: 2 }}>
           <SearchIcon />
@@ -66,15 +103,22 @@ export default function GitHubUser() {
         {loading ? (
           <CircularProgress />
         ) : error ? (
-          <Alert severity="error">{error}</Alert>
-        ) : (
+          <Box>
+            {badRequest(error) ? (
+              <Alert severity="info">"This gitHub user does not exist"</Alert>
+            ) : (
+              <Alert severity="error">{error}</Alert>
+            )}
+          </Box>
+        ) : Object.keys(data).length != 0 ? (
           <Container
             fixed
-            sx={{
-              border: 1,
-              boxShadow: 1,
-              pb: 2,
-            }}
+            sx={
+              {
+                // border: 1,
+                // boxShadow: 1,
+              }
+            }
           >
             <Box>
               <Grid container alignItems="center">
@@ -102,15 +146,50 @@ export default function GitHubUser() {
                   </Typography>
                 </Grid>
               </Grid>
-
-              {/* <Grid item xs={4}>
-                <h1 style={{ background: "red" }}>xs=4</h1>
+              <Grid container sx={{ textAlign: "left", mt: 4 }}>
+                <Grid item xs={12} sm={3} md={2}>
+                  <Typography variant="h5">Biography:</Typography>
+                </Grid>
+                <Grid
+                  item
+                  md={10}
+                  sm={9}
+                  sx={{ mt: 1, maxHeight: "150px", overflowY: "auto" }}
+                >
+                  <Typography sx={{ textAlign: "left" }}>{data.bio}</Typography>
+                </Grid>
               </Grid>
-              <Grid item xs={8}>
-                <h1 style={{ background: "green" }}>xs=8</h1>
-              </Grid> */}
+              <Grid container sx={{ textAlign: "left", mt: 4 }}>
+                <Grid item sm={3}>
+                  <Typography variant="h5">Number of followers:</Typography>
+                </Grid>
+                <Grid item sm={8}>
+                  <Typography sx={{ textAlign: "left", mt: 1 }}>
+                    {data.followers}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container sx={{ textAlign: "left", mt: 4 }}>
+                <Grid item sm={4}>
+                  <Typography variant="h5">
+                    Number of published repositories:
+                  </Typography>
+                </Grid>
+                <Grid item sm={8}>
+                  <Typography sx={{ textAlign: "left", mt: 1 }}>
+                    {data.public_repos}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Typography variant="h5" sx={{ textAlign: "left", mt: 4 }}>
+                List of repositories:
+              </Typography>
+
+              <TableRepos rows={data.list_repos} />
             </Box>
           </Container>
+        ) : (
+          <></>
         )}
       </Box>
     </div>
---
+++ src/utiles/tabla.jsx
@@ -0,0 +1,199 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import { visuallyHidden } from "@mui/utils";
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Name of repository",
+  },
+  {
+    id: "created_at",
+    numeric: true,
+    disablePadding: false,
+    label: "Creation Date",
+  },
+  {
+    id: "description",
+    numeric: true,
+    disablePadding: false,
+    label: "Description",
+  },
+];
+
+function EnhancedTableHead(props) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+};
+
+export default function TableRepos(props) {
+  const { rows } = props;
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("created_at");
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(rows, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      ),
+    [order, orderBy, page, rowsPerPage]
+  );
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <TableContainer>
+        <Table
+          sx={{ minWidth: 750 }}
+          aria-labelledby="tableTitle"
+          size="medium"
+        >
+          <EnhancedTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+          />
+          <TableBody>
+            {visibleRows.map((row, index) => {
+              const labelId = `enhanced-table-checkbox-${index}`;
+
+              return (
+                <TableRow
+                  hover
+                  role="checkbox"
+                  tabIndex={-1}
+                  key={row.name}
+                  sx={{ cursor: "pointer" }}
+                >
+                  <TableCell
+                    component="th"
+                    id={labelId}
+                    scope="row"
+                    padding="none"
+                  >
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.created_at}</TableCell>
+                  <TableCell align="right">{row.description}</TableCell>
+                </TableRow>
+              );
+            })}
+            {emptyRows > 0 && (
+              <TableRow
+                style={{
+                  height: 53 * emptyRows,
+                }}
+              >
+                <TableCell colSpan={3} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Box>
+  );
+}
